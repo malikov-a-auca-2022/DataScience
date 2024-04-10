5d1128098fc6ceb980f50b8c67c61d526bdcf49c
Adilet Malikov <malikov_a@auca.kg>
2024-04-10 13:06:58 +0600

    Read files

--- lab3/lab3_5.py
+++ lab3/lab3_5.py
@@ -1,1 +1,26 @@
 import pandas as pd
+
+# water (GDP.FWTL)
+# air_pol (ATM)
+# cancer (DYN)
+# gdp (GPD.MKTP)
+# industry (IND)
+# mortality (STA)
+# tobacco (PRV)
+
+files = {'air_pol': 'API_EN.ATM.PM25.MC.ZS_DS2_en_excel_v2_3588.xls',
+         'water': 'API_ER.GDP.FWTL.M3.KD_DS2_en_excel_v2_3500.xls',
+         'industry': 'API_NV.IND.TOTL.KD.ZG_DS2_en_excel_v2_2566.xls',
+         'gdp': 'API_NY.GDP.MKTP.KD.ZG_DS2_en_excel_v2_67.xls',
+         'cancer': 'API_SH.DYN.NCOM.ZS_DS2_en_excel_v2_3058.xls',
+         'tobacco': 'API_SH.PRV.SMOK_DS2_en_excel_v2_3887.xls',
+         'mortality': 'API_SH.STA.AIRP.MA.P5_DS2_en_excel_v2_3403.xls'}
+
+data = {}
+for name, file in files.items():
+    df = pd.read_excel('xmls/' + file, sheet_name='Data', skiprows=3)
+    df = df.drop(df.columns[[0, 2, 3]], axis=1)
+    df = df.transpose()
+
+    data[name] = df
+
